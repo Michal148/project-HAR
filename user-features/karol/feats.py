83288d0c84156d2d997e69380277afce4e4245d7
Karol Nosek <karnn022@gmail.com>
2023-01-12 12:23:10 +0100

    Kinda chetead 1/4 and 3/4

--- user-features/karol/feats.py
+++ user-features/karol/feats.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import math
 from scipy import fftpack
+import matplotlib.pyplot as plt
 # df = pd.read_csv("Accelerometer.csv")
-# f = df.drop(['time', 'seconds_elapsed'], axis=1)
+# df = df.drop(['time', 'seconds_elapsed'], axis=1)
 
 
 # Calculating mean value
@@ -50,28 +51,20 @@ def energy(dataf):
     return lst
 
 
-# Calculating fft from signal and converting resulting list into data frame
+# Calculating fft from signal
 def fourier(data, freq=100):
-    n = len(data)
+    arr = pd.DataFrame.to_numpy(data)
+    arr = np.transpose(arr)
+    n = len(arr[0])
     fs = freq
-    freq_vec = [fs/n * x for x in range(n)]
-    dff = pd.DataFrame(np.abs(fftpack.fft(data)))
-    dff.columns = ['x', 'y', 'z']
-    return freq_vec, dff
-
-
-# Calculating interquartile range
-def iqr(dataf):
-    cols = ['x', 'y', 'z']
     lst = []
-    vec, dataff = fourier(dataf)
-
-    for col in cols:
-        q1 = np.percentile(dataff[col].sort_values(), 25, method='midpoint')
-        q3 = np.percentile(dataff[col].sort_values(), 75, method='midpoint')
-        lst.append(q1-q3)
-    return lst
-
+    fvec = [fs / n * x for x in range(int(n / 2))]
+    for i in range(len(arr)):
+        fur = np.abs(fftpack.fft(arr[i]) / n)
+        fur = fur[:int(n / 2)]
+        fur[1:-1] = fur[1:-1]
+        lst.append(fur)
+    return fvec, lst
 
 
 # Calculating Mean Power Frequency
@@ -85,25 +78,48 @@ def mpf(dataf):
     return lst
 
 
-# Calculating one quarter of frequency (especially needs revision)
+# Calculating one quarter of frequency (needs a check)
 def one_quarter(dataf):
     fvec, dff = fourier(dataf)
-    cols = ['x', 'y', 'z']
+    fvec = np.array(fvec)
     lst = []
-    for col in cols:
-        value = sum(p for p in dff[col].iloc[int(0.25*len(dff)):])
-        lst.append(value)
+    for i in range(len(dff)):
+        arr = np.cumsum(dff[i])
+        norm_arr = arr / arr[-1]
+        for j in range(1, len(arr)):
+            if norm_arr[j] < 0.25:
+                continue
+            if round(norm_arr[j],2) >= 0.25:
+                lst.append(fvec[j])
+                break
     return lst
 
 
-# Calculating three quarters of frequency (needs a revison)
+# Calculating three quarters of frequency (needs a check)
 def three_quarters(dataf):
     fvec, dff = fourier(dataf)
-    cols = ['x', 'y', 'z']
+    fvec = np.array(fvec)
     lst = []
-    for col in cols:
-        value = sum(p for p in dff[col].iloc[0:int(0.75 * len(dff))])
-        lst.append(value)
+    for i in range(len(dff)):
+        arr = np.cumsum(dff[i])
+        norm_arr = arr / arr[-1]
+        for j in range(1, len(arr)):
+            if norm_arr[j] < 0.75:
+                continue
+            if round(norm_arr[j], 2) >= 0.75:
+                lst.append(fvec[j])
+                break
     return lst
 
-# print(three_quarters(df))
+
+# Calculating interquartile range (needs a check)
+def iqr(dataf):
+    cols = ['x', 'y', 'z']
+    lst = []
+    q1 = one_quarter(dataf)
+    q3 = three_quarters(dataf)
+    for i in range(len(q1)):
+        lst.append(q3[i] - q1[i])
+    return lst
+
+
