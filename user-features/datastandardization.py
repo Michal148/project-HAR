1cf7b283d1cfee457403cf0064426fd0864d7dd4
Kasper Kornak <kasper.kornak@gmail.com>
2023-02-15 21:03:50 +0100

    ios conversion

---
+++ user-features/datastandardization.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def ios_conversion(data):
+    if data['platform'][0] == "apple":
+        return data
+    
+    else:
+        data['x'] = data['x'] / 9.80665
+        data['y'] = data['z'] / 9.80665
+        data['z'] = -data['z'] / 9.80665
+        data['mag'] = data['mag'] / 9.80665
+        
+        data['lagX0'] = data['lagX0'] / 9.80665
+        data['lagY0'] = data['lagY0'] / 9.80665
+        data['lagZ0'] = data['lagZ0'] / 9.80665
+
+        data['lagX1'].iloc[1:] = data['lagX1'].iloc[1:].astype(float) / 9.80665
+        data['lagY1'].iloc[1:] = data['lagY1'].iloc[1:].astype(float) / 9.80665
+        data['lagZ1'].iloc[1:] = data['lagZ1'].iloc[1:].astype(float) / 9.80665
+
+        data['lagX2'].iloc[2:] = data['lagX2'].iloc[2:].astype(float) / 9.80665
+        data['lagY2'].iloc[2:] = data['lagY2'].iloc[2:].astype(float) / 9.80665
+        data['lagZ2'].iloc[2:] = data['lagZ2'].iloc[2:].astype(float) / 9.80665
+
+        if 'jerkX' in data.columns:
+            data['jerkX'].iloc[:-1] = data['jerkX'].iloc[:-1].astype(float) / 9.80665
+            data['jerkY'].iloc[:-1] = data['jerkY'].iloc[:-1].astype(float)  / 9.80665
+            data['jerkZ'].iloc[:-1] = data['jerkZ'].iloc[:-1].astype(float) / 9.80665
+            data['jerkMag'].iloc[:-1] = data['jerkMag'].iloc[:-1].astype(float)  / 9.80665
+
+        return data
+        
---
+++ user-features/streaming-work-dir/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find . -name ".DS_Store" -type f -delete
+find . -name "Accelerometer.csv" -type f -delete
+find . -name "Gyroscope.csv" -type f -delete
+find . -name "Magnetoneter.csv" -type f -delete
+find . -name "Annotation.csv" -type f -delete
+find . -name "Metadata.csv" -type f -delete
+find . -name "Gravity.csv" -type f -delete
+find . -name "Orientation.csv" -type f -delete
+find . -name "TotalAcceleration.csv" -type f -delete
